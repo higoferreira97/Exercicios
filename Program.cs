14b462fc46ff04eb5a6908e0b278ac953c58ce4e
higoferreira97 <higoferreira.dev@gmail.com>
2025-07-07 21:04:28 -0300

    Feat: Feito exercicio usando for, if, else e array

--- Program.cs
+++ Program.cs
@@ -706,10 +706,31 @@ namespace ExercicioUm
 
 
 
+            Console.Write("Informe um número: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                int x = int.Parse(line[0]);
+                int y = int.Parse(line[1]);
+
+                if (y == 0)
+
+                    Console.WriteLine("Divisão impossivel");
+
+                else
+                {
+                    double div = (double)x / y;
+                    Console.WriteLine(div.ToString("F1", CultureInfo.InvariantCulture));
+                }
+
+            }
+
+                //----------------------------------------------------------------
 
 
 
-            //----------------------------------------------------------------
 
 
 
@@ -717,12 +738,9 @@ namespace ExercicioUm
 
 
 
-
-
-
+            }
         }
     }
-}
 
 
 
